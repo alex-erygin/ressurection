6352a5fdb4a9021053aa03e1cc184b64f3bebbf1
alex-erygin <alex.erygin@gmail.com>
2024-09-25 15:56:56 +0300

    Агент на GigaChat

---
+++ gigachain/main.py
@@ -0,0 +1,21 @@
+"""Пример работы с чатом через gigachain"""
+from langchain.schema import HumanMessage, SystemMessage
+from langchain.chat_models.gigachat import GigaChat
+
+# Авторизация в сервисе GigaChat
+chat = GigaChat(credentials='', verify_ssl_certs=False)
+
+messages = [
+    SystemMessage(
+        content="Ты эмпатичный бот-психолог, который помогает пользователю решить его проблемы."
+    )
+]
+
+while(True):
+    # Ввод пользователя
+    user_input = input("User: ")
+    messages.append(HumanMessage(content=user_input))
+    res = chat(messages)
+    messages.append(res)
+    # Ответ сервиса
+    print("Bot: ", res.content)
